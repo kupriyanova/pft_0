27c1050b65c29870e5c913d4a68b412e0153629f
Ekaterina Kupriyanova <ek.kupriyanova@gmail.com>
2022-04-04 16:41:55 +0300

    Task 7
    Научиться запускать тесты в разных браузерах

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,14 +3,23 @@ package ru.stqa.pft.addressbook.appmanager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private final String browser;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public SessionHelper getSessionHelper() {
         return sessionHelper;
     }
@@ -31,8 +40,17 @@ public class ApplicationManager {
     }
 
     public void init() {
-        WebDriverManager.chromedriver().setup();
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            WebDriverManager.firefoxdriver().setup();
+            wd = new FirefoxDriver();
+        } else if (browser.equals(BrowserType.CHROME)) {
+            WebDriverManager.chromedriver().setup();
+            wd = new ChromeDriver();
+        } else if (browser.equals(BrowserType.IE)) {
+            WebDriverManager.iedriver().setup();
+            wd = new InternetExplorerDriver();
+        }
+
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php");
         sessionHelper = new SessionHelper(wd);
