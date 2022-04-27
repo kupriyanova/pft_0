54ad9ad341ff894753a1847bec633b66eb568fd8
Ekaterina Kupriyanova <ek.kupriyanova@gmail.com>
2022-04-27 14:46:38 +0300

    Task 21

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,15 +1,19 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -52,15 +56,22 @@ public class ApplicationManager {
 
         dbHelper = new DBHelper();
 
-        if (browser.equals(BrowserType.FIREFOX)) {
-            WebDriverManager.firefoxdriver().setup();
-            wd = new FirefoxDriver();
-        } else if (browser.equals(BrowserType.CHROME)) {
-            WebDriverManager.chromedriver().setup();
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            WebDriverManager.iedriver().setup();
-            wd = new InternetExplorerDriver();
+        if (properties.getProperty("selenium.server").equals("")) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                WebDriverManager.firefoxdriver().setup();
+                wd = new FirefoxDriver();
+            } else if (browser.equals(BrowserType.CHROME)) {
+                WebDriverManager.chromedriver().setup();
+                wd = new ChromeDriver();
+            } else if (browser.equals(BrowserType.IE)) {
+                WebDriverManager.iedriver().setup();
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
 
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
