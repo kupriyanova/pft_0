1d7bc2bed72e160921744c217e57152e9a541c5e
Ekaterina K <ek.kupriyanova@gmail.com>
2022-06-17 17:38:45 +0300

    Task 18 finish

--- mantis/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -23,6 +23,7 @@ public class ApplicationManager {
     private FtpHelper ftpHelper;
     private MailHelper mailHelper;
     private JamesHelper jamesHelper;
+    private DbHelper dbHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -69,6 +70,13 @@ public class ApplicationManager {
         if(jamesHelper == null)
             jamesHelper = new JamesHelper(this);
         return jamesHelper;
+    }
+
+    public DbHelper db() {
+        if (dbHelper == null) {
+            dbHelper = new DbHelper(this);
+        }
+        return dbHelper;
     }
 
     public WebDriver getDriver() {
