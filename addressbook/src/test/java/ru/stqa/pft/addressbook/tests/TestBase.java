3c6eb0e5f52e069e51460073205521c21109b735
Ekaterina K <ek.kupriyanova@gmail.com>
2022-06-26 23:26:56 +0300

    Task 21 allure

--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -3,10 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
@@ -22,7 +20,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-
+@Listeners(MyTestListener.class)
 public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(GroupCreationTests.class);
@@ -30,8 +28,9 @@ public class TestBase {
             System.getProperty("browser",  BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUp() throws IOException {
+    public void setUp(ITestContext context) throws IOException {
         app.init();
+        context.setAttribute("app", app);
     }
     @BeforeMethod
     public void logTestStart(Method m, Object[] p) {
